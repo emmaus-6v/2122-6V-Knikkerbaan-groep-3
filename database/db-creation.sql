cbcf16a0e1476ddb5c83bc53d2918c64f0385d5f
Valentijn.vw <valentijnvanwinden@gmail.com>
2022-01-12 19:15:45 +0000

    Database structuur

--- database/db-creation.sql
+++ database/db-creation.sql
@@ -9,19 +9,21 @@ CREATE TABLE sensorData (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  aantalKnikkers INTEGER
+  key TEXT NOT NULL,
+  value INTEGER TEXT NOT NULL
 );
 
 CREATE TABLE instellingen (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  wachttijdPoort INTEGER NOT NULL
+  key TEXT NOT NULL,
+  value TEXT NOT NULL
 );
 
 
 /* Indien je standaard wat gegevens in de database wilt,
    voeg hieronder dan INSERT regels to */
 INSERT INTO runs (stamp) VALUES (CURRENT_TIMESTAMP);
-INSERT INTO sensorData (run, stamp, aantalKnikkers) VALUES (1, CURRENT_TIMESTAMP, 3);
-INSERT INTO instellingen (run, stamp, wachttijdPoort) VALUES (1, CURRENT_TIMESTAMP, 15);
+INSERT INTO sensorData (run, stamp, key, value) VALUES (1, CURRENT_TIMESTAMP, 'aantal_knikkers', 3);
+INSERT INTO instellingen (run, stamp, key, value) VALUES (1, CURRENT_TIMESTAMP, 'wachttijd_poort', 15);
